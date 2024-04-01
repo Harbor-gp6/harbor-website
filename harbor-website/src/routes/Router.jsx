ed2890fb6278fa74d3b46ca26dd5986ae20de7c0
Luka-Caetano <luka.santos@sptech.school>
2024-04-01 18:19:25 -0300

    harbor-website set up

---
+++ harbor-website/src/routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/Home";
+
+export const Router = createBrowserRouter([
+    {
+        path: '/',
+        element:(
+            <HomePage/>
+        )
+    }
+])
