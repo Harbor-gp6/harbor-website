a4667a578809bf2c1ac3df07c6a03cbc0c0879b6
Leozinho11 <leoferraribarreto@hotmail.com>
2024-04-07 02:05:19 -0300

    Create the typography component

---
+++ harbor-website/src/components/Typography/Typography.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export function Typography({ children }) {
+  return (
+    <h6 className='text-xl text-white max-w-screen-md text-center'>
+      {children}
+    </h6>
+  )
+}
