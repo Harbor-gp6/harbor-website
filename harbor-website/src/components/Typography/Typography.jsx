1cac6b90bbec2fbd85954bed03178f146402e564
Leozinho11 <leoferraribarreto@hotmail.com>
2024-04-16 20:55:06 -0300

    Update components

--- harbor-website/src/components/Typography/Typography.jsx
+++ harbor-website/src/components/Typography/Typography.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-export function Typography({ children, color }) {
+export function Typography({ children, color, textPosition }) {
   return (
-    <h6 className={`text-xl text-${color || 'white'} max-w-screen-md text-center`}>
+    <h6 className={`text-xl text-${color || 'white'} max-w-screen-md text-${textPosition || 'center'}`}>
       {children}
     </h6>
   )
