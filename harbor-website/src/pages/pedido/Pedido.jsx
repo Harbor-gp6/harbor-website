b73343d38e319f6ade6aa601cded1954d169c720
Vitor Ramos <vitoramosc@gmail.com>
2024-05-11 23:08:56 -0300

    criando a tela de pedido

---
+++ harbor-website/src/pages/pedido/Pedido.jsx
@@ -0,0 +1,10 @@
+import { Typography } from "../../components/Typography/Typography";
+
+export default function Pedido(){
+
+    return (
+        <div className="w-full flex items-center justify-center">
+            <Typography color='black'> Pedido </Typography>
+        </div>
+    )
+}
--- harbor-website/src/routes/Router.jsx
+++ harbor-website/src/routes/Router.jsx
@@ -5,6 +5,7 @@ import { Footer } from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Register from '../pages/register/Register'
 import RegisterForm from '../pages/register/RegisterForm'
+import Pedido from "../pages/pedido/Pedido";
 
 const Layout = () => {
   const location = useLocation();
@@ -18,6 +19,7 @@ const Layout = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/cadastrar' element={<Register />} />
         <Route path='/cadastro' element={<RegisterForm />} />
+        <Route path='/pedido' element={<Pedido />} />
       </Routes>
       {path !== "/login" && path !== '/cadastrar' && path !== '/cadastro' && <Footer />}
     </>
